d07e5bb5264d46c0a9b32d204e44efe712c1e41b
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-08-17 20:34:33 +0300

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+    attr_accessor :anagrams
+
+    def initialize(word)
+        @word = word
+        self.anagrams = []
+    end
+
+    def match(words_to_compare)
+        words_to_compare.each do |array_word|
+            if array_word.chars.sort == self.word.chars.sort
+                self.anagrams.push(array_word)
+            end
+        end
+        self.anagrams
+    end
+end
